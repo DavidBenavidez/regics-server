6d4665e4f6de402a99feaa897682a28bd463a48a
skysemilla <sylegaspi@up.edu.ph>
2018-03-19 23:22:24 +0800

    Added form validation

--- src/entities/course/router.js
+++ src/entities/course/router.js
@@ -47,31 +47,76 @@ router.delete('/api/course/delete/:course_no', async (req, res) => {
 
 // Add Course
 router.post('/api/course/add', async (req, res) => {
-  try {
-    const id = await Ctrl.addCourse(req.body);
-
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully added course',
-      data: id
-    });
-  } catch (status) {
-    res.status(500).json({ status });
+ if (
+    req.body.course_name &&
+    req.body.section &&
+    req.body.class_size &&
+    req.body.sais_class_count &&
+    req.body.sais_waitlisted_count &&
+    req.body.actual_count &&
+    req.body.course_date &&
+    req.body.course_time_start &&
+    req.body.course_time_end &&
+    req.body.minutes &&
+    req.body.units &&
+    (req.body.is_lab == 'true' ||
+      req.body.is_lab == 'false') &&  
+    req.body.room_no &&
+    req.body.empno
+  ){
+    try {
+      const id = await Ctrl.addCourse(req.body);
+      res.status(200).json({
+        status: 200,
+        message: 'Successfully created course',
+        data: id
+      });
+    } catch (status) {
+      res.status(500).json({ status });
+    }
+  } else {
+    res.status(400).json({ status: 400 });
   }
 });
 
 // edit course
 router.put('/api/course/edit', async (req, res) => {
-  try {
-    await Ctrl.editCourse(req.body);
-    const user = await Ctrl.getCourse({ course_no: req.body.course_no });
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully edited course',
-      data: user
-    });
-  } catch (status) {
-    res.status(status).json({ status });
+  if (
+    req.body.course_no &&
+    req.body.course_name &&
+    req.body.section &&
+    req.body.class_size &&
+    req.body.sais_class_count &&
+    req.body.sais_waitlisted_count &&
+    req.body.actual_count &&
+    req.body.course_date &&
+    req.body.course_time_start &&
+    req.body.course_time_end &&
+    req.body.minutes &&
+    req.body.units &&
+    (req.body.is_lab == 'true' ||
+      req.body.is_lab == 'false') && 
+    (req.body.course_status == 'dissolved' ||
+      req.body.course_status == 'petitioned' ||
+      req.body.course_status == 'addition' ||
+      req.body.course_status == 'approved') && 
+    req.body.reason &&
+    req.body.room_no &&
+    req.body.empno
+  ){
+    try {
+      await Ctrl.editCourse(req.body);
+      const user = await Ctrl.getCourse({ course_no: req.body.course_no });
+      res.status(200).json({
+        status: 200,
+        message: 'Successfully edited course',
+        data: user
+      });
+    } catch (status) {
+      res.status(status).json({ status });
+    }
+  } else {
+    res.status(400).json({ status: 400 });
   }
 });
 
