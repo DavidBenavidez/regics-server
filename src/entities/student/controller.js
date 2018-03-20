b4c885730927e2e465c795cf02ba5468352e8ca1
DavidBenavidez <ddbenavidez2@up.edu.ph>
2018-03-20 14:43:37 +0800

    Fixed student API

--- src/entities/student/controller.js
+++ src/entities/student/controller.js
@@ -6,7 +6,7 @@ export const getAllStudents = () => {
   return new Promise((resolve, reject) => {
     const queryString = `
         SELECT
-          a.student_no, a.name, a.status, a.curriculum, a.classification, b.name AS adviser
+          a.student_no, a.name, a.status, a.student_curriculum, a.classification, b.name AS adviser
         FROM
           student a, system_user b
         WHERE
