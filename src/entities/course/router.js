1719fd1556d52160b9977668b8fdbe80bdc76928
DavidBenavidez <ddbenavidez2@up.edu.ph>
2018-04-09 23:46:58 +0800

    Fixed add user error

--- src/entities/course/router.js
+++ src/entities/course/router.js
@@ -71,33 +71,4 @@ router.put('/api/course/edit', async (req, res) => {
   }
 });
 
-// Add User
-router.post('/api/users', async (req, res) => {
-  if (
-    req.body.name &&
-    req.body.username &&
-    req.body.password &&
-    req.body.password == req.body.confirm_password &&
-    (req.body.system_position == 'faculty' ||
-      req.body.system_position == 'head' ||
-      req.body.system_position == 'member') &&
-    (req.body.status == 'resigned' ||
-      req.body.status == 'on_leave' ||
-      req.body.status == 'active')
-  ) {
-    try {
-      const id = await Ctrl.addUser(req.body);
-      res.status(200).json({
-        status: 200,
-        message: 'Successfully created user',
-        data: id
-      });
-    } catch (status) {
-      res.status(status).json({ status });
-    }
-  } else {
-    res.status(400).json({ status: 400 });
-  }
-});
-
 export default router;
