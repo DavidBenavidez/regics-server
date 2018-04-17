d8f0d3f00888e2179bb028446ca159ff8e2bc34d
Kate Salvador <kateanne419@gmail.com>
2018-04-17 15:28:50 +0800

    updated get suggested adviser

--- src/entities/user/controller.js
+++ src/entities/user/controller.js
@@ -349,23 +349,44 @@ export const deleteAdviserAdvisee = (session_user, { id }) => {
   });
 };
 
-// stdnt num, empno,
-//
+export const getSuggestedAdviser = () => {
+  return new Promise((resolve, reject) => {
+    var noOfAdviseeArray = getAllAdviseeClassification(); //{{empno, noOfAdvisee},{empno, noOfAdvisee}}
+    var suggestedAdviserArray = {}; // array of top 3 profs w/ least number of advisee
 
-// export const getProfInfo = ({ empno }) => {
-//   return new Promise((resolve, reject) => {
-//     const queryString = `
-//         SELECT name, system_position, status, teaching_load FROM system_user WHERE empno = ?`;
+    /* ipasok mo muna lahat sa suggestedArray then sort mo then kunin mo ung index 0-3 */
+    noOfAdviseeArray.sort(function(a, b) {
+      return a.total - b.total;
+    });
 
-//     db.query(queryString, empno, (err, rows) => {
-//       if (err) {
-//         console.log(err);
-//         return reject(500);
-//       }
-//       if (!rows.length) {
-//         return reject(404);
-//       }
-//       return resolve(rows);
-//     });
-//   });
-// };
+    var count = 0;
+    var status;
+    var profName;
+    for (var i = 0; i < noOfAdviseeArray.length; i++) {
+      if (count == 3)
+        //if there are already top 3 advisers
+        return resolve(suggestedAdviserArray);
+
+      /* query if active */
+      const queryString = `SELECT name, status from system_user where empno=?`;
+      db.query(queryString, noOfAdviseeArray[i].empno, (err, row) => {
+        if (err) {
+          console.log(err);
+          return reject(500);
+        }
+        if (!row.length) {
+          return reject(404);
+        }
+        status = row[0].status;
+        profName = row[0].name;
+      });
+      /* query end */
+
+      if (status == 'active') {
+        noOfAdviseeArray[i].name = profName; //add the name of the prof
+        suggestedAdviserArray.push(noOfAdviseeArray[i]); //add the obj to the suggestedArray
+        count++;
+      }
+    }
+  });
+};
