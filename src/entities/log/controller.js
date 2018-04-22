050d1c2de09b71cb5e9673033f3d5f49a247939f
DavidBenavidez <ddbenavidez2@up.edu.ph>
2018-04-22 20:56:52 +0800

    Betsy the best

--- src/entities/log/controller.js
+++ src/entities/log/controller.js
@@ -3,10 +3,17 @@ import db from '../../database';
 export const getLogs = () => {
   return new Promise((resolve, reject) => {
     const query = `
-      SELECT *
-      FROM log_data
+      SELECT
+        log_no, 
+        DATE_FORMAT(log_timestamp, '%M %d %Y %h:%i %p') AS log_timestamp,
+        log_action,
+        log_user
+      FROM 
+        log_data
+      ORDER BY
+        log_timestamp
+      DESC
     `;
-
     db.query(query, (err, row) => {
       if (err) {
         console.log(err.message);
